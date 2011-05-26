13c7ed674b77c61757ed151ca09ad5cd703154e1
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-05-26 17:11:32 +0800

    AEq instances for Quantity and Vec.

---
+++ Numeric/Units/Dimensional/AEq.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+-- | Provides instances of @Data.AEq.AEq@ from the ieee754 library.
+module Numeric.Units.Dimensional.AEq where
+
+import Numeric.Units.Dimensional (Dimensional (Dimensional), Quantity)
+import Numeric.Units.Dimensional.LinearAlgebra.Vector (Vec (ListVec))
+import Data.AEq
+
+
+deriving instance AEq a => AEq (Quantity d a)
+
+instance (Floating a, AEq a) => AEq (Vec ds a)  -- CPos et al
+  where
+    ListVec xs === ListVec ys = and $ zipWith (===) xs ys
+    ListVec xs ~== ListVec ys = and $ zipWith (~==) xs ys
