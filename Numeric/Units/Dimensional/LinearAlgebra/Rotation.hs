6b8197b841eefe5be92d37f7b74d0ab7318955ba
Bjorn Buckwalter <bjorn@buckwalter.se>
2013-09-13 13:56:32 +0200

    Rotation matrix for rotation about arbitrary vector.

--- Numeric/Units/Dimensional/LinearAlgebra/Rotation.hs
+++ Numeric/Units/Dimensional/LinearAlgebra/Rotation.hs
@@ -28,16 +28,28 @@ type Homo33 d = Mat ((d:*:d:*.d) :*:
 -- to rotating the coordinate system in opposite direction).
 
 rotX :: Floating a => PlaneAngle a -> Homo33 DOne a
-rotX a = consRow   (vCons _1 $ vCons _0      $ vSing _0)
-       $ consRow   (vCons _0 $ vCons (cos a) $ vSing (negate (sin a)))
-       $ rowMatrix (vCons _0 $ vCons (sin a) $ vSing (cos a))
+rotX a =   (_1 <:    _0 <:.            _0 )
+       |:  (_0 <: cos a <:. negate (sin a))
+       |:. (_0 <: sin a <:.         cos a )
 
 rotY :: Floating a => PlaneAngle a -> Homo33 DOne a
-rotY a = consRow   (vCons (cos a)          $ vCons _0 $ vSing (sin a))
-       $ consRow   (vCons _0               $ vCons _1 $ vSing _0)
-       $ rowMatrix (vCons (negate (sin a)) $ vCons _0 $ vSing (cos a))
+rotY a =   (        cos a  <: _0 <:. sin a)
+       |:  (           _0  <: _1 <:.    _0)
+       |:. (negate (sin a) <: _0 <:. cos a)
 
 rotZ :: Floating a => PlaneAngle a -> Homo33 DOne a
-rotZ a = consRow   (vCons (cos a) $ vCons (negate (sin a)) $ vSing _0)
-       $ consRow   (vCons (sin a) $ vCons (cos a)          $ vSing _0)
-       $ rowMatrix (vCons _0      $ vCons _0               $ vSing _1)
+rotZ a =   (cos a <: negate (sin a) <:. _0)
+       |:  (sin a <:         cos a  <:. _0)
+       |:. (   _0 <:            _0  <:. _1)
+
+-- | @rotV v a@ generates the rotation matrix for a counterclockwise
+-- rotation through the angle @a@ about an axis in R^3, which is
+-- determined by the arbitrary unit vector @v@ that has its initial
+-- point at the origin (from Anton, Rorres, 1994, pages 190-191).
+rotV :: Floating a => Homo3 DOne a -> Angle a -> Homo33 DOne a
+rotV v t =   (a*a*omct +   cos t <: a*b*omct - c*sin t <:. a*c*omct + b*sin t)
+         |:  (b*a*omct + c*sin t <: b*b*omct +   cos t <:. b*c*omct - a*sin t)
+         |:. (c*a*omct - b*sin t <: c*b*omct + a*sin t <:. c*c*omct +   cos t)
+  where
+    (a,b,c) = toTuple v
+    omct    = _1 - cos t
